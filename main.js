c7a6df823dd2ec2fd33e9d6d1503e4d659e878d2
Eduaney Augusto da Silva <67763275+skiksplinter@users.noreply.github.com>
2021-10-25 20:57:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'skiksplinter',
+  youtube: 'eduZinhouU',
+  facebook: 'eduaney',
+  instagram: 'eduaney',
+  twitter: 'eduZinhouU',
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class');
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
+  }
+}
+
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userPhoto.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+getGitHubProfileInfos();
